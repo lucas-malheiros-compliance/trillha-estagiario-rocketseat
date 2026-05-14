140c839ca156a6bf201642c68cd9d67082ffdc42
Lucas Malheiros <lucas.malheiros@compliancesolucoes.com.br>
2026-05-14 12:05:15 -0300

    Adicionando conteúdo de aulas de Bundlers

---
+++ nivel-06-javascript/03-javascript-antes-do-framework/09-bundlers/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const HTMLWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: path.resolve(__dirname, "src", "js", "index.js"),
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist", "index.html"),
+        },
+        port: 3000,
+        open: true,
+    },
+    plugins: [new HTMLWebPackPlugin()],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+            exclude: "/node_modules",
+        },
+        {
+            test: /\.js$/i,
+            exclude: "/node_modules",
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [["@babel/preset-env", { targets: "defaults" }]],
+                },
+            },
+        },
+        ],
+    },
+}
