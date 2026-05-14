89d39102d15cdd9303419909bb3a145402011795
Lucas Malheiros <lucas.malheiros@compliancesolucoes.com.br>
2026-05-14 16:08:21 -0300

    Adição de interação/manipulação ao forms por meio do Javascript

---
+++ nivel-06-javascript/03-javascript-antes-do-framework/projeto-hairday/src/modules/form/submit.js
@@ -0,0 +1,44 @@
+import dayjs from "dayjs"
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
+
+// data atual para o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Configurando a data padrão e a data mínima
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    try {
+        const name = clientName.value.trim()
+        if (!name) {
+            return alert("Informe o nome do cliente!")
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+        if (!hourSelected) {
+            return alert("Selecione a hora!")
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        const id = new Date().getTime()
+
+        // console.log({
+        //     id, name, when
+        // });
+
+
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
+
+}
