46db67fb744dc0317d0d493f477cd12579fed9f1
Lucas Malheiros <lucas.malheiros@compliancesolucoes.com.br>
2026-05-15 11:34:18 -0300

    Finalizando projeto Hairday

--- nivel-06-javascript/03-javascript-antes-do-framework/projeto-hairday/src/modules/form/hours-load.js
+++ nivel-06-javascript/03-javascript-antes-do-framework/projeto-hairday/src/modules/form/hours-load.js
@@ -6,18 +6,22 @@ import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
 
     hours.innerHTML = ""
+
+    const unavaliableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
 
     const opening = openingHours.map((hour) => {
         const [scheduleHour] = hour.split(":")
 
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+
+        const avaliable = !unavaliableHours.includes(hour) && !isHourPast
 
         return {
             hour,
-            avaliable: isHourPast
+            avaliable
         }
 
     })
--- nivel-06-javascript/03-javascript-antes-do-framework/projeto-hairday/src/modules/form/submit.js
+++ nivel-06-javascript/03-javascript-antes-do-framework/projeto-hairday/src/modules/form/submit.js
@@ -1,4 +1,8 @@
 import dayjs from "dayjs"
+
+import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js"
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -10,7 +14,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
     event.preventDefault()
 
     try {
@@ -30,11 +34,16 @@ form.onsubmit = (event) => {
 
         const id = new Date().getTime()
 
-        // console.log({
-        //     id, name, when
-        // });
+        await scheduleNew(
+            {
+                id,
+                name,
+                when
+            }
+        )
 
-
+        await schedulesDay()
+        clientName.value = ""
 
     } catch (error) {
         alert("Não foi possível realizar o agendamento.")
